6b0e7ca43515c2442f69b9df661d30aef7bef964
shaqdeff <shaquillendunda@gmail.com>
2022-12-15 00:52:17 +0300

    Create cursor set helper function

---
+++ src/helpers/cursorSet.js
@@ -0,0 +1,32 @@
+// Description: Get cursor coordinates
+const getCursorCoordinates = () => {
+  let x, y;
+  if (isSupported) {
+    const selection = window.getSelection();
+    // Check if there is a selection (i.e. cursor in place)
+    if (selection.rangeCount !== 0) {
+      // Clone the range
+      const range = selection.getRangeAt(0).cloneRange();
+      // Collapse the range to the start, so there are not multiple chars selected
+      range.collapse(false);
+      // getCientRects returns all the positioning information we need
+      const rect = range.getClientRects()[0];
+      if (rect) {
+        x = rect.left;
+        y = rect.top;
+      }
+    }
+  }
+  return { x, y };
+};
+
+// Description: Set cursor to end of element
+const setCursorToEnd = (element) => {
+  const range = document.createRange();
+  const selection = window.getSelection();
+  range.selectNodeContents(element);
+  range.collapse(false);
+  selection.removeAllRanges();
+  selection.addRange(range);
+  element.focus();
+};
