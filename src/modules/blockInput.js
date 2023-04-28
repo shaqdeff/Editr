e0a3fcdee1cee05c4e80e2d832c69bac40e8c2a0
shaqdeff <shaquillendunda@gmail.com>
2023-04-28 22:51:45 +0300

    Add keydown eventListeners for showing and removing popup

--- src/modules/blockInput.js
+++ src/modules/blockInput.js
@@ -1,1 +1,19 @@
 import { showPopup, removePopup } from './popup.js';
+
+const blockInput = (block = document.querySelector('.block')) => {
+  block.addEventListener('keydown', (e) => {
+    const popup = document.querySelector('.popup');
+
+    // show popup
+    if (e.key === '/' && !popup) {
+      showPopup(block);
+    }
+
+    // remove popup
+    if (e.key === 'Escape' || e.key === 'Backspace') {
+      removePopup();
+    }
+  });
+};
+
+export default blockInput;
